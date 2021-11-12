143b70a93bd338135373d281421273026a609255
Antoine MARIE <antoine.marie62126@gmail.com>
2021-11-12 17:03:15 +0100

    > php bin/console make:migrations

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/movie')]
 class MovieController extends AbstractController
 {
     #[Route('/', name: 'movie_index', methods: ['GET'])]
